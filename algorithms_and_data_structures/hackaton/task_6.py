8c2e4c7f593fa30238cf5da7f503cf865a6bbc6a
Sergo <gitkhartmann@gmail.com>
2023-10-13 19:28:04 +0300

    feat: (algo) добавлено решение задачи №6

---
+++ algorithms_and_data_structures/hackaton/task_6.py
@@ -0,0 +1,21 @@
+# Посчитать количество людей у которых в почте присутствуют цифры в таблице people
+
+# Файл - https://drive.google.com/file/d/1XtJbF512x3QYygOa8Wn_NaB0nhgxy2OS/view?usp=sharing
+# create table people(name text, phone_number text, address text, date_of_birth text, email text, job text, credit_card_number text, ipv4 text, url text, company text, user_agent text, country text);
+
+import psycopg2
+
+try:
+    conn = psycopg2.connect(database='lesson5_db', user='sergo', password='197612', host='localhost', port='5432')
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT email FROM people;")
+
+    data = cursor.fetchall()
+
+    data = [n for n, in data if any(ch.isdigit() for ch in n)]
+    print(len(data))
+
+except:
+    print('Can`t connection to database')
