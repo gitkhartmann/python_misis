0ca54f7d2bc4ae16af894a43b5fcbe90b577ed32
Sergo <gitkhartmann@gmail.com>
2023-11-07 15:23:39 +0300

    feat: (python-functions) исправлено решение задачи 5

--- functions/task_5.py
+++ functions/task_5.py
@@ -17,10 +17,10 @@
 def is_power(num, d=2):
     if num == 1:
         return True
-    elif num < d:
+    elif num < d or d == 1:
         return False
     else:
         return is_power(num / d, d)
 
 
-print(is_power(1000, 10))
+print(is_power(3, 1))
